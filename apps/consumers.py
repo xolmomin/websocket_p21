b1c50694cfb4b6866af6ceda624c406c87df00b0
xolmomin <xolmomin@gmail.com>
2024-09-21 18:21:33 +0500

    add sending file

--- apps/consumers.py
+++ apps/consumers.py
@@ -1,11 +1,19 @@
 import ujson
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from django.contrib.auth.models import User
 from django.forms import model_to_dict
 
-from apps.models import Message
+from apps.models import Message, Room
 
 
 class CustomAsyncJsonWebsocketConsumer(AsyncJsonWebsocketConsumer):
+
+    async def receive(self, text_data=None, bytes_data=None, **kwargs):
+        if not isinstance(text_data, dict):
+            await self.send_json({'message': 'xabar json bolishi shart'})
+            return
+        return await super().receive(text_data, bytes_data, **kwargs)
 
     @classmethod
     async def decode_json(cls, text_data):
@@ -17,105 +25,144 @@ class CustomAsyncJsonWebsocketConsumer(AsyncJsonWebsocketConsumer):
 
 
 class ChatConsumer(CustomAsyncJsonWebsocketConsumer):
-    group_name = 'chat'
 
-    async def save_msg(self, content) -> Message:
-        return await Message.objects.acreate(
-            message=content.get('message'),
-            author=self.user,
-            file_id=content.get('file')
-        )
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_name = None
+        self.room_group_name = None
+        self.room = None
+        self.user = None
+        self.user_inbox = None
 
-    async def check_user(self) -> bool:
+    async def is_authenticate(self) -> bool:
         if self.user.is_anonymous:
-            await self.send_json({'msg': 'login is required!'})
+            await self.send_json({'message': 'login is required!'})
             await self.disconnect(0)
             await self.close()
             return False
         return True
 
-    async def notify_status(self, is_connected: bool = True) -> None:
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                "type": "notification.message",
-                "message": f"{self.user.username} is {('offline', 'online')[is_connected]} !",
-                "from_user": model_to_dict(self.user, ['id', 'username']),
-            }
-        )
-
-    async def connect(self) -> None:
+    async def connect(self):
         self.user = self.scope['user']
-        # Join room group
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        # connection has to be accepted
         await self.accept()
-
-        if not await self.check_user():
+        if not await self.is_authenticate():
             return
+
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        self.room = await Room.objects.aget(name=self.room_name)
+        self.user_inbox = f'inbox_{self.user.id}'
+
+        # # join the room group
+        # await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        # send the user list to the newly joined user
+        # user_list = []
+        # async for user in self.room.online.all():
+        #     user_list.append(user.username)
+        #
+        # await self.send_json({
+        #     'type': 'user_list',
+        #     'users': user_list,
+        # })
+
+        # create a user inbox for private messages
+        await self.channel_layer.group_add(self.user_inbox, self.channel_name)
+
+        # send the join event to the room
         await self.notify_status()
+        # await self.room_update_user(self.room, self.user)
+
+    async def notify_status(self, is_connected: bool = True):
+        pass
+        # await self.channel_layer.group_send(
+        #     self.room_group_name,
+        #     {
+        #         'type': 'user_join',
+        #         'user': model_to_dict(self.user, ('id', 'username')),
+        #     }
+        # )
+
+    # @sync_to_async
+    # def room_update_user(self, room, user, action: str = 'add'):
+    #     getattr(room.online, action)(user)
 
     async def disconnect(self, close_code):
-        # Leave room group
-        if self.user.is_authenticated:
-            await self.notify_status(False)
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+        # delete the user inbox for private messages
+        await self.channel_layer.group_discard(self.user_inbox, self.channel_name, )
 
-    # Receive message from WebSocket
+        # # send the leave event to the room
+        # await self.channel_layer.group_send(
+        #     self.room_group_name,
+        #     {
+        #         'type': 'user_leave',
+        #         'user': self.user.username,
+        #     }
+        # )
+        # self.room_update_user(self.room, self.user, 'remove')
+
     async def receive_json(self, content, **kwargs):
-        keys = {'message', 'file'}
-        if len(set(content) & keys) < 1:
-            await self.send_json({'message': f'Shu kalitlardan birini yuborish shart {keys}'})
+        if not (len(set(content) & {'target', 'type'}) > 1):
+            await self.send_json({'message': 'xabar yuboriladigan user idni kiriting'})
             return
 
-        # Send message to room group
-        msg = await self.save_msg(content)
+        if content['type'] == 'private':
+            msg = await Message.objects.acreate(user=self.user, room=self.room, text=content['message'])
+            # send private message to the target
+            await self.channel_layer.group_send(
+                f"inbox_{content['target']}",
+                {
+                    'type': 'private_message',
+                    'user': model_to_dict(self.user, ('id', 'username')),
+                    'message': model_to_dict(msg, ('id', 'text')),
+                }
+            )
+            # send private message delivered to the user
+            await self.send_json({
+                'type': 'chat_message',
+                'target': content['target'],
+                'message': model_to_dict(msg, ('id', 'text')),
+            })
+            return
 
+        # send chat message event to the room
         await self.channel_layer.group_send(
-            self.group_name,
+            self.room_group_name,
             {
-                "type": "chat.message",
-                "message": model_to_dict(msg, ['id', 'message', 'file']),
-                "from_user": model_to_dict(self.user, ['id', 'username']),
+                'type': 'chat_message',
+                'user': model_to_dict(self.user, ('id', 'username')),
+                'message': content['message'],
             }
         )
 
-    async def notification_message(self, event):
-        response = {
-            "message": event.get('message'),
-            "file": event.get('file'),
-            "from_user": event["from_user"]['username']
-        }
-        if self.user.id != event["from_user"]['id']:
-            await self.send_json(response)
-        else:
-            if isinstance(event['message'], dict):
-                await self.send_json(event['message'] | {"status": "xabar yuborildi"})
-
-    # Receive message from room group
     async def chat_message(self, event):
-        # Send message to WebSocket
-        response = {
-            "message": event["message"]['message'],
-            "file": event['message']['file'],
-            "from_user": event["from_user"]['username']
-        }
-        if self.user.id != event["from_user"]['id']:
-            await self.send_json(response)
-        else:
-            if isinstance(event['message'], dict):
-                await self.send_json(event['message'] | {"status": "xabar yuborildi"})
+        await self.send_json(event)
+
+    async def user_join(self, event):
+        await self.send_json(event)
+
+    async def user_leave(self, event):
+        await self.send_json(event)
+
+    async def private_message(self, event):
+        await self.send_json(event)
+
+    async def private_message_delivered(self, event):
+        await self.send_json(event)
 
 
 '''
 
-ujson +
-Botir online bo'ldi +
-Botir chiqib ketdi +
+string1
+ws://localhost:8000/ws/chat/p21/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMxMzI4NzEyLCJpYXQiOjE3MjY5MjIzMTIsImp0aSI6IjkxM2I2OTgwMWU4NjQ0NDg4MGZlZDhlZWNiZDI4ZDQwIiwidXNlcl9pZCI6Mn0.htTAe3EZh5ZKno286tCuyDijBWH-FCGCu3luA2jyaCo
 
-required fields +
-fayl yuborish + 
 
-1-1
+string2
+ws://localhost:8000/ws/chat/p21/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMxMzI4OTIwLCJpYXQiOjE3MjY5MjI1MjAsImp0aSI6IjRhNGJmMTVkZjRjYjRhYjM4Y2Q5ZmIwYmQ0OTk5ODMxIiwidXNlcl9pZCI6M30.hxkXmMl3qmDvwb8oDDEAfq3ft5H_OYr24dfi6V_m_04
 
+
+string3
+ws://localhost:8000/ws/chat/p21/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMxMzMwMTYxLCJpYXQiOjE3MjY5MjM3NjEsImp0aSI6IjFkYzk2NzhhNGMzZTQ0MmY5NWEzOTE1NTg0NDg2MjdjIiwidXNlcl9pZCI6NH0.LpSO6iDYm8Qz9h0ymcyV_NpFAClPBdmmmmTx_7R_R6Y
 
 '''
--- apps/models.py
+++ apps/models.py
@@ -1,4 +1,6 @@
-from django.db.models import Model, DateTimeField, TextField, ForeignKey, CASCADE, FileField, SET_NULL, OneToOneField
+from django.contrib.auth.models import User
+from django.db.models import Model, DateTimeField, ForeignKey, CASCADE, FileField, CharField, ManyToManyField, \
+    SlugField, BooleanField
 from django.db.models.functions import Now
 
 
@@ -6,13 +8,55 @@ class TimeBasedModel(Model):
     updated_at = DateTimeField(auto_now=True)
     created_at = DateTimeField(auto_now_add=True, db_default=Now())
 
+    class Meta:
+        abstract = True
+
+
+class SlugBasedModel(Model):
+    title = CharField(max_length=255)
+    slug = SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        abstract = True
+
+
+class DeleteBasedModel(Model):
+    is_deleted = BooleanField(db_default=False)
+
+    class Meta:
+        abstract = True
+
 
 class Attachment(TimeBasedModel):
     file = FileField(upload_to='attachments/%Y/%m/%d/')
 
 
-class Message(Model):
-    message = TextField(null=True, blank=True)
-    file = OneToOneField('apps.Attachment', SET_NULL, null=True, blank=True)
-    author = ForeignKey('auth.User', CASCADE, related_name='messages')
-    created_at = DateTimeField(auto_now_add=True)
+class Room(Model):
+    name = CharField(max_length=128)
+    online = ManyToManyField('auth.User', blank=True)
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def __str__(self):
+        return f'{self.name} ({self.get_online_count()})'
+
+
+class Message(TimeBasedModel):
+    user = ForeignKey('auth.User', CASCADE)
+    room = ForeignKey('apps.Room', CASCADE)
+    text = CharField(max_length=512)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.text} [{self.created_at}]'
